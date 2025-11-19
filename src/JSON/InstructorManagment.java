1862cd826082fc3eb5c1de89a7ed88e5bcd5c0aa
ritage <ritageashraf2005@gmail.com>
2025-11-19 22:42:27 +0200

    update instructor service class

--- src/JSON/InstructorManagment.java
+++ src/JSON/InstructorManagment.java
@@ -120,6 +120,42 @@ public class InstructorManagment {
     Student s = studentService.getStudentById(studentId);
     return s.getQuizScores();  
 }
+public double getLessonAverage(String courseId, String lessonId) {
+    ArrayList<String> students = courseService.getEnrolledStudents(courseId);
+
+    int total = 0;
+    int count = 0;
+
+    for (String sid : students) {
+        Student s = studentService.getStudentById(sid);
+        Integer score = s.getQuizScores().get(lessonId);
+
+        if (score != null) {
+            total += score;
+            count++;
+        }
+    }
+
+    return count == 0 ? 0 : (double) total / count;
+}
+
+
+   public double getStudentCourseCompletionPercentage(String studentId, String courseId) {
+    Student student = studentService.getStudentById(studentId);
+    Course course = courseService.getCourseById(courseId);
+
+    if (student == null || course == null) return 0.0;
+    int totalLessons = course.getLessons().size();
+    if (totalLessons == 0) return 0.0;
+    List<String> completedLessons = student.getProgress().getOrDefault(courseId, new ArrayList<>());
+    //.getOrDefault(key, defaultValue) is a Java Map method that:
+     //Tries to get the value for the key (courseId)
+//If the key is not present (the student has not completed any lessons in this course yet), 
+//it returns the default value, which is a new empty list.
+    int completedCount = completedLessons.size();
+    return (completedCount * 100.0) / totalLessons;
+}
+
 
    }
 
