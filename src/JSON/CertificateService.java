77ced055d2a73e025a3e8d24a8a9071c3d6e0af5
EngYoussefSharaf <sharafyoussef693@gmail.com>
2025-11-20 13:13:18 +0200

    certificatePDF

--- src/JSON/CertificateService.java
+++ src/JSON/CertificateService.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package JSON;
 
-/**
- *
- * @author Mega Store
- */
-public class CertificateService {
-    
+import Courses.Certificate;
+import Courses.Course;
+import Users.Student;
+import Users.User;
+import java.io.IOException;
+
+public class CertificateService extends JsonDatabaseManager<User> {
+
+    public CertificateService(String fileName) throws IOException {
+        super(fileName, User.class);
+    }
+
+    public Certificate generateCertificate(Student student, Course course) {
+        String certId = "CERT-" + System.currentTimeMillis();
+        String date = java.time.LocalDate.now().toString();
+
+        Certificate cert = new Certificate(certId, student.getUserID(), course.getCourseId(), date);
+        if(SearchStudent(student.getUserID()) == -1)
+        {
+            return null;
+        }
+        //student.getCertificates().add(cert); // i need this method in Student
+        db.add(SearchStudent(student.getUserID()), student);
+        save();
+        // CertificatePDFGenerator.generate(cert, student, course);
+        return cert;
+    }
+
+    public int SearchStudent(String id) {
+        int i=0;
+        for(User u : db) {
+            if(u.getUserID().equals(id)) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
 }
