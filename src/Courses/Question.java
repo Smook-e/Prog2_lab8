9b5f883401239bedf61335f4efe47df476269afa
sara-0011 <sarahabiiba@icloud.com>
2025-11-20 09:50:11 +0200

    commit to change Question location

--- src/Courses/Question.java
+++ src/Courses/Question.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package JSON;
+package Courses;
 
 import java.util.List;
 
@@ -58,6 +58,6 @@ public class Question {
     @Override
     public String toString()
     {
-        return "Question(" +questionId+ ")"+text;
+        return "Question(" +questionId+ "): "+text;
     }
 }
